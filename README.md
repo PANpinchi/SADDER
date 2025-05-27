243fc4d3b1f2054d89336376894429e20a52556d
PANpinchi <87637733+PANpinchi@users.noreply.github.com>
2025-05-27 11:31:15 +0800

    init

--- README.md
+++ README.md
@@ -28,6 +28,25 @@ pip install -r dependencies.txt
 pip install opencv-contrib-python
 
 pip install tifffile
+
+git clone https://github.com/PANpinchi/BARIS-ERA.git
+
+cd ./BARIS-ERA
+
+pip install -v -e .
+
+pip install mmcv-full==1.5.3 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.12.0/index.html
+
+pip install terminaltables
+pip install pycocotools
+pip install scikit-learn
+pip install numpy==1.23.5
+pip install gdown
+pip install mmcls
+pip install yapf==0.40.1
+pip install natsort
+
+cd ..
 ```
 
 Run the commands below to download the pre-trained CPD model:
--- install_environment.sh
+++ install_environment.sh
@@ -5,9 +5,20 @@ conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit
 
 echo "✅ Installing additional Python dependencies..."
 pip install -r dependencies.txt
-
 pip install opencv-contrib-python
-
 pip install tifffile
 
+git clone https://github.com/PANpinchi/BARIS-ERA.git
+cd ./BARIS-ERA
+pip install -v -e .
+pip install mmcv-full==1.5.3 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.12.0/index.html
+pip install terminaltables
+pip install pycocotools
+pip install scikit-learn
+pip install numpy==1.23.5
+pip install gdown
+pip install mmcls
+pip install yapf==0.40.1
+pip install natsort
+cd ..
 echo "🎉 All packages installed successfully!"
